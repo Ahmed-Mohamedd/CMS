89f950c05469fc259fd0a8b48b3c6b14b7b1edd8
Aya <ayasa3d5@gmail.com>
2025-05-14 00:00:55 +0300

    persontype

---
+++ CMS.Application/Features/PersonTypes/Commands/CreatePersonType/CreatePersonTypeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CMS.Application.Common.CQRS;
+using CMS.Domain.Entities;
+using CMS.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Application.Features.PersonTypes.Commands.CreatePersonType
+{
+    public class CreatePersonTypeCommandHandler(IApplicationDbContext context)
+        : ICommandHandler<CreatePersonTypeCommand, CreatePersonTypeResult>
+    {
+        public async Task<CreatePersonTypeResult> Handle(CreatePersonTypeCommand request, CancellationToken cancellationToken)
+        {
+            var personType = new PersonType
+            {
+                Name = request.PersonType.Name
+            };
+            await context.PersonTypes.AddAsync(personType);
+            await context.SaveChangesAsync(cancellationToken);
+            return new CreatePersonTypeResult(personType.Id);
+        }
+    }
+}
