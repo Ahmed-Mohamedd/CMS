89464c4ced997633db96e1c51e1e69bdeba48554
Aya <ayasa3d5@gmail.com>
2025-05-14 00:53:34 +0300

    soldier

---
+++ CMS.Api/Controllers/SoldierController.cs
@@ -0,0 +1,27 @@
+﻿using CMS.Application.Features.Soldiers.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SoldierController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SoldierController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSoldierByMilitartServiceEndDate(DateTime militaryServiceEndDate)
+        {
+            var command = new GetSoldierByMilitaryServiceEndDateQuery(militaryServiceEndDate);
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ CMS.Application/Features/Soldiers/Queries/GetSoldierByMilitaryServiceEndDateQuery.cs
@@ -0,0 +1,14 @@
+﻿using CMS.Application.Common.CQRS;
+using CMS.Application.Features.Soldiers.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Application.Features.Soldiers.Queries
+{
+    public record GetSoldierByMilitaryServiceEndDateQuery(
+        DateTime MilitaryServiceEndDate) : IQuery<SoldierDto>;
+    
+}
