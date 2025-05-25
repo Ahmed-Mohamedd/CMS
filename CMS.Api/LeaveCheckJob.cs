c40f75483c37bfffad117cca7393b64ee838cc17
ahmed mohamed <ahmedmohamedabdelaty1@outlook.com>
2025-05-25 20:08:09 +0300

    UpdatingPersonAbsenceStatus

---
+++ CMS.Api/LeaveCheckJob.cs
@@ -0,0 +1,19 @@
+﻿using CMS.Domain.Interfaces;
+
+namespace CMS.Api
+{
+    public class LeaveCheckJob
+    {
+        private readonly ILeaveService _updater;
+
+        public LeaveCheckJob(ILeaveService updater)
+        {
+            _updater = updater;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            await _updater.UpdateLeaveStatusAsync(CancellationToken.None);
+        }
+    }
+}
